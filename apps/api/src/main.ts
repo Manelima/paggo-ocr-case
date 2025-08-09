e1fde03684872c871e3a133955a8539b47b71033
Emanuel <emanuellima4321@gmail.com>
2025-08-09 10:00:44 -0300

    fix(api): resolving main.ts PORT exclusion

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,5 +1,4 @@
 // apps/api/src/main.ts
-
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe, Logger } from '@nestjs/common';
@@ -9,17 +8,25 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
+  const whitelist = [
+    'http://localhost:3000', 
+    process.env.FRONTEND_URL,  
+  ];
+
   app.enableCors({
-    origin: 'http://localhost:3000', 
+    origin: function (origin, callback) {
+      if (!origin || whitelist.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
     credentials: true,
   });
+  // -----------------------------
 
   const port = process.env.API_PORT || 3001;
-
   await app.listen(port);
-  Logger.log(
-    ` Application is running on: http://localhost:${port}`,
-    'Bootstrap',
-  );
+  Logger.log(`Application is running on: http://localhost:${port}`, 'Bootstrap');
 }
 bootstrap();
